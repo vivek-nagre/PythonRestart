d0358ebaf45ea9c540c40e037915e8c9b19d62b3
vivek-nagre <viveknagre198@gmail.com>
2024-03-24 00:39:14 +0530

    Identifier in python

--- identifier.py
+++ identifier.py
@@ -1,1 +1,7 @@
 print("All about Identifier and variables in python")
+# variable / identifier used to store the data in it 
+# Dyanamicaly typed python will take care of memory allocation 
+#  We were not allowed to use the reserve keyword as our variable/identifier 
+import keyword
+print(keyword.kwlist)
+print(len(keyword.kwlist))  # 35 key words 
