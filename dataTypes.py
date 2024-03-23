2addf70aab42fd7a4f603eb39ccc303d7fff710d
vivek-nagre <viveknagre198@gmail.com>
2024-03-24 00:53:45 +0530

    Datatypes in python

---
+++ dataTypes.py
@@ -0,0 +1,16 @@
+print("Data Types in Python")
+#  there are 
+
+'''
+Python contains the following inbuilt data types
+1. int -- To store the integer value
+2. float -- To store the decimal value 
+3.bool -- To store True False
+4.str -- TO store string 
+5.range -- Used during loop
+9.list -- To store the data in list we can manipulate 
+10.tuple -- > Non Mutable (we can only use)
+11.set --> Unique Data
+13.dict -- > To have the Key value pair in 
+14.None -- > assign None type value and then we can put the flag on it 
+'''
